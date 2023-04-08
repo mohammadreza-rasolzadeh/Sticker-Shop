99812750787aebfcac83bc4fcff555f4f83451b9
mojtaba <mojtaba75rs@gmail.com>
2023-04-08 16:36:20 +0430

    Installing Tailwind CSS on the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-
 const App = () => {
-  return ( 
-    <div>
-      Sticker Shop
-    </div>
-   );
-}
- 
+  return <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>;
+};
+
 export default App;
