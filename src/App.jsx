d885655b116cbb8be0d02a87790c8af9a8a099d7
mojtaba <mojtaba75rs@gmail.com>
2023-04-10 18:22:15 +0430

    Implementation of Product Slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline text-red-500 text-center mt-12">Hello world!</h1>;
 };
 
 export default App;
---
+++ src/reducers/productSlice.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchProducts = createAsyncThunk(
+    "products/fetchProducts",
+    async() => {
+        try {
+            const response = await axios.get("http://localhost:9000/products");
+            return response.data;
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+);
+
+const initialState = {
+    items: [],
+    status: null,
+};
+
+const productsSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers: {},
+    extraReducers(builder) {
+        builder
+            .addCase(fetchProducts.pending, (state, action) => {
+                state.status = "pending";
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.status = "success";
+                state.items = action.payload;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.status = "rejected";
+            });
+    },
+});
+
+export default productsSlice.reducer;
