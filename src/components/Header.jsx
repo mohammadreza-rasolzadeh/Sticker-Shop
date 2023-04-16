24d5d26217592b905758a6cba6f8578e63de0030
mojtaba <mojtaba75rs@gmail.com>
2023-04-16 15:48:29 +0430

    Making the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+    return ( 
+        <div>
+            <h1 className="leading-relaxed text-4xl font-extrabold text-center text-palette-primary mt-4 py-2 sm:py-4">
+               استیکرهای جذاب برنامه نویسی
+            </h1>
+            <p className="max-w-xl text-center px-2 mx-auto text-base">برنامه نویسی که روی لپ تاپش استکیر نداشته باشه برنامه نویس نیست 😝</p>
+        </div>
+     );
+}
+ 
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            height: (theme) => ({
+                112: "28rem",
+                120: "30rem",
+            }),
+            minHeight: (theme) => ({
+                80: "20rem",
+            }),
+            colors: {
+                palette: {
+                    lighter: "#f5f3ff",
+                    light: "#ddd6fe",
+                    primary: "#5b21b6",
+                    dark: "#4c1d95",
+                },
+            },
+            fontFamily: {
+                primary: ['tanha']
+            }
+        },
     },
     plugins: [],
-}
+};
